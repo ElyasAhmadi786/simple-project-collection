6860a02819005456d7952738f7cee279f6f14ecc
ElyasAhmadi786 <168517319+ElyasAhmadi786@users.noreply.github.com>
2025-09-05 19:34:51 +0430

    Create home_page.dart

---
+++ flutter_application_1/lib/home_page.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'main.dart';
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blueGrey,
+      appBar: AppBar(
+        iconTheme: IconThemeData(color: Colors.white),
+        title: const Text("Main Page",
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.blueGrey[800],
+      ),
+      drawer: RusableDrawer(),
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('images/hack6.jpg'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Center(
+          child: Container(
+            padding: EdgeInsets.all(20),
+            decoration: BoxDecoration(
+              color: Colors.black.withOpacity(0.5),
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Text(
+              'Welcome to App',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 24,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
